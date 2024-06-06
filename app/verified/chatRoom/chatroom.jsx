3ea95751096b9575297ddec0e88b1de744cb1796
Shafihu Mustapha <shafihumustapha0101@gmail.com>
2024-06-06 09:41:52 +0000

    fix: chatroom

--- app/verified/chatRoom/chatroom.jsx
+++ app/verified/chatRoom/chatroom.jsx
@@ -9,11 +9,30 @@ const chatroom = () => {
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
         <Header />
         <KeyboardAvoidingView
-            style={{flex: 1, backgroundColor: 'gray'}}
+            style={{flex: 1, backgroundColor: '#F8F8FF'}}
             behavior={Platform.OS === "ios" ? "padding" : "height"}
             keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}>
-            <ScrollView contentContainerStyle={{flexGrow: 1, justifyContent: 'flex-end', padding: 10,}}>
-                <Text>Chatroom</Text>
+            <ScrollView contentContainerStyle={{flexGrow: 1, justifyContent: 'flex-end', padding: 10, gap: 8}}>
+                <View className="bg-white w-[95%] p-2 rounded-lg gap-2">
+                    <View>
+                        <Text className="text-[#00BFFF]">ME</Text>
+                    </View>
+                    <View>
+                        <Text className="tracking-wider font-normal text-[16px] text-gray-800">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                        </Text>
+                    </View>
+                </View>
+                <View className="bg-white w-[95%] p-2 rounded-lg gap-2">
+                    <View>
+                        <Text className="text-red-500">MY AI</Text>
+                    </View>
+                    <View>
+                        <Text className="tracking-wider font-normal text-[16px] text-gray-800">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                        </Text>
+                    </View>
+                </View>
             </ScrollView>
             <Bottom />
         </KeyboardAvoidingView>
